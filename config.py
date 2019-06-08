05cf5a92c0c4cc674d8886874d8daf85bd2c4a82
tielemao <tieleyumao@gmail.com>
2019-06-08 21:54:44 +0800

    flask db

--- config.py
+++ config.py
@@ -2,6 +2,10 @@
 # _*_ coding: utf-8 _*_
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
